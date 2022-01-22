348c93c6df61417748f7a9efeb2cf631e9b16d9d
Taisei Toya <tytaisei0102@gmail.com>
2022-01-22 15:16:46 +0100

    Characterクラスを実装

---
+++ cpp04/ex03/Character.cpp
@@ -0,0 +1,72 @@
+#include "Character.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Character::Character(std::string name) : name_(name), index_{0} {
+  for (size_t i = 0; i < k_max_materias_size; i++) {
+    materias_[i] = NULL;
+  }
+};
+
+Character::Character(const Character& src) { *this = src; }
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Character::~Character() {
+  for (size_t i = 0; i < k_max_materias_size; i++) {
+    delete materias_[i];
+  }
+}
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Character& Character::operator=(Character const& rhs) {
+  if (this != &rhs) {
+    this->name_ = rhs.name_;
+    for (size_t i = 0; i < k_max_materias_size; i++) {
+      delete this->materias_[i];
+      this->materias_[i] = rhs.materias_[i]->clone();
+    }
+  }
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& o, Character const& i) {
+  std::cout << "I am " << i.getName() << std::endl;
+  return o;
+}
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+void Character::equip(AMateria* m) {
+  materias_[index_] = m;
+  index_++;
+  index_ = index_ == k_max_materias_size ? 0 : index_;
+};
+
+void Character::unequip(int idx) {
+  if (idx < k_max_materias_size) {
+    delete materias_[idx];
+    materias_[idx] = NULL;
+  }
+};
+
+void Character::use(int idx, ICharacter& target) {
+  if (idx < k_max_materias_size && materias_[idx]) {
+    materias_[idx]->use(target);
+  }
+};
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+/* ************************************************************************** */
---
+++ cpp04/ex03/Character.hpp
@@ -0,0 +1,35 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <iostream>
+#include <string>
+
+#include "ICharacter.hpp"
+
+class Character : public ICharacter {
+ public:
+  Character(std::string name);
+  Character(Character const& src);
+  ~Character();
+
+  Character& operator=(Character const& rhs);
+
+  std::string const& getName() const;
+  void equip(AMateria* m);
+  void unequip(int idx);
+  void use(int idx, ICharacter& target);
+
+ private:
+  Character();
+
+  static const size_t k_max_materias_size = 4;
+
+  std::string name_;
+  AMateria* materias_[k_max_materias_size];
+  size_t index_;
+};
+
+std::ostream& operator<<(std::ostream& o, Character const& i);
+
+#endif /* ******************************************************* CHARACTER_H \
+        */
