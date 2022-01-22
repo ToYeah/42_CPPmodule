4f8a2e8569d1a59da4be6a33c46e180ea536f5c1
Taisei Toya <tytaisei0102@gmail.com>
2022-01-22 15:49:11 +0100

    MateriaSourceを実装

---
+++ cpp04/ex03/MateriaSource.cpp
@@ -0,0 +1,63 @@
+#include "MateriaSource.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+MateriaSource::MateriaSource() : index_(0) {
+  for (size_t i = 0; i < k_max_materias_size; i++) {
+    materias_[i] = NULL;
+  }
+}
+
+MateriaSource::MateriaSource(const MateriaSource& src) { *this = src; }
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+MateriaSource::~MateriaSource() {
+  for (size_t i = 0; i < k_max_materias_size; i++) {
+    delete materias_[i];
+  }
+}
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+MateriaSource& MateriaSource::operator=(MateriaSource const& rhs) {
+  if (this != &rhs) {
+    for (size_t i = 0; i < k_max_materias_size; i++) {
+      delete this->materias_[i];
+      this->materias_[i] = rhs.materias_[i]->clone();
+    }
+  }
+  return *this;
+}
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+void MateriaSource::learnMateria(AMateria* m) {
+  delete materias_[index_];
+  materias_[index_] = m;
+  index_++;
+  index_ = index_ == k_max_materias_size ? 0 : index_;
+};
+
+AMateria* MateriaSource::createMateria(std::string const& type) {
+  for (size_t i = 0; i < k_max_materias_size; i++) {
+    if (materias_[i] && materias_[i]->getType() == type) {
+      return materias_[i]->clone();
+    }
+  }
+  return NULL;
+};
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+/* ************************************************************************** */
---
+++ cpp04/ex03/MateriaSource.hpp
@@ -0,0 +1,30 @@
+#ifndef MATERIASOURCE_HPP
+#define MATERIASOURCE_HPP
+
+#include <iostream>
+#include <string>
+
+#include "IMateriaSource.hpp"
+
+class MateriaSource : public IMateriaSource {
+ public:
+  MateriaSource();
+  MateriaSource(MateriaSource const& src);
+  ~MateriaSource();
+
+  void learnMateria(AMateria* m);
+  AMateria* createMateria(std::string const& type);
+
+  MateriaSource& operator=(MateriaSource const& rhs);
+
+ private:
+  static const size_t k_max_materias_size = 4;
+
+  AMateria* materias_[k_max_materias_size];
+  size_t index_;
+};
+
+std::ostream& operator<<(std::ostream& o, MateriaSource const& i);
+
+#endif /* *************************************************** MATERIASOURCE_H \
+        */
