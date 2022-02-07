ca7f48894a213b688f4a699013855db73d2523e2
Taisei Toya <tytaisei0102@gmail.com>
2022-02-07 11:04:33 +0100

    テストを修正

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -7,21 +7,30 @@ int main() {
   std::srand(static_cast<unsigned int>(std::time(NULL)));
 
   Bureaucrat taro = Bureaucrat("Taro", 1);
-  Bureaucrat hanako = Bureaucrat("Hanako", 100);
+  Bureaucrat hanako = Bureaucrat("Hanako", 150);
 
   std::cout << "-----RobotomyRequestForm Test----" << std::endl;
 
   try {
-    std::cout << "Succes: " << std::endl;
+    std::cout << "\nSucces-----" << std::endl;
     RobotomyRequestForm form = RobotomyRequestForm("Target");
-    form.signForm(taro);
+    taro.signForm(form);
     taro.executeForm(form);
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
 
   try {
-    std::cout << "Sign Error: " << std::endl;
+    std::cout << "\nSign Error----- " << std::endl;
+    RobotomyRequestForm form = RobotomyRequestForm("Target");
+    hanako.signForm(form);
+    hanako.executeForm(form);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  try {
+    std::cout << "\nNot Sign Error----- " << std::endl;
     RobotomyRequestForm form = RobotomyRequestForm("Target");
     taro.executeForm(form);
   } catch (const std::exception& e) {
@@ -29,9 +38,9 @@ int main() {
   }
 
   try {
-    std::cout << "Grade Error: " << std::endl;
+    std::cout << "\nGrade Error----- " << std::endl;
     RobotomyRequestForm form = RobotomyRequestForm("Target");
-    form.signForm(taro);
+    taro.signForm(form);
     hanako.executeForm(form);
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
