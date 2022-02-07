ee6e9af39a758af30cb3fd18496d82bcb15b50a3
Taisei Toya <tytaisei0102@gmail.com>
2022-02-07 20:16:13 +0100

    テストを追加

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -1,6 +1,7 @@
 #include <ctime>
 
 #include "Bureaucrat.hpp"
+#include "Intern.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
@@ -10,114 +11,52 @@ int main() {
 
   Bureaucrat taro = Bureaucrat("Taro", 1);
   Bureaucrat hanako = Bureaucrat("Hanako", 150);
-
-  std::cout << "\n-----RobotomyRequestForm Test----" << std::endl;
+  Intern intern = Intern();
 
   try {
-    std::cout << "\nSucces-----" << std::endl;
-    RobotomyRequestForm form = RobotomyRequestForm("Target");
-    taro.signForm(form);
-    taro.executeForm(form);
+    Form* form;
+    form = intern.makeForm("robotomy request", "Target");
+    if (form) {
+      taro.signForm(*form);
+      taro.executeForm(*form);
+      delete form;
+    }
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
 
   try {
-    std::cout << "\nSign Error----- " << std::endl;
-    RobotomyRequestForm form = RobotomyRequestForm("Target");
-    hanako.signForm(form);
-    hanako.executeForm(form);
+    Form* form;
+    form = intern.makeForm("shrubbery creation", "Target");
+    if (form) {
+      taro.signForm(*form);
+      taro.executeForm(*form);
+      delete form;
+    }
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
 
   try {
-    std::cout << "\nNot Sign Error----- " << std::endl;
-    RobotomyRequestForm form = RobotomyRequestForm("Target");
-    taro.executeForm(form);
+    Form* form;
+    form = intern.makeForm("presidential pardon", "Target");
+    if (form) {
+      taro.signForm(*form);
+      taro.executeForm(*form);
+      delete form;
+    }
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
 
   try {
-    std::cout << "\nGrade Error----- " << std::endl;
-    RobotomyRequestForm form = RobotomyRequestForm("Target");
-    taro.signForm(form);
-    hanako.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  std::cout << "\n-----RobotomyRequestForm Test----" << std::endl;
-
-  try {
-    std::cout << "\nSucces-----" << std::endl;
-    PresidentialPardonForm form = PresidentialPardonForm("Target");
-    taro.signForm(form);
-    taro.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  try {
-    std::cout << "\nSign Error----- " << std::endl;
-    PresidentialPardonForm form = PresidentialPardonForm("Target");
-    hanako.signForm(form);
-    hanako.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  try {
-    std::cout << "\nNot Sign Error----- " << std::endl;
-    PresidentialPardonForm form = PresidentialPardonForm("Target");
-    taro.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  try {
-    std::cout << "\nGrade Error----- " << std::endl;
-    PresidentialPardonForm form = PresidentialPardonForm("Target");
-    taro.signForm(form);
-    hanako.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  std::cout << "\n-----ShrubberyCreationForm Test----" << std::endl;
-
-  try {
-    std::cout << "\nSucces-----" << std::endl;
-    ShrubberyCreationForm form = ShrubberyCreationForm("Target");
-    taro.signForm(form);
-    taro.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  try {
-    std::cout << "\nSign Error----- " << std::endl;
-    ShrubberyCreationForm form = ShrubberyCreationForm("Target");
-    hanako.signForm(form);
-    hanako.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  try {
-    std::cout << "\nNot Sign Error----- " << std::endl;
-    ShrubberyCreationForm form = ShrubberyCreationForm("Target");
-    taro.executeForm(form);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << '\n';
-  }
-
-  try {
-    std::cout << "\nGrade Error----- " << std::endl;
-    ShrubberyCreationForm form = ShrubberyCreationForm("Target");
-    taro.signForm(form);
-    hanako.executeForm(form);
+    Form* form;
+    form = intern.makeForm("RobotomyRequestForm", "Target");
+    if (form) {
+      taro.signForm(*form);
+      taro.executeForm(*form);
+      delete form;
+    }
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
   }
