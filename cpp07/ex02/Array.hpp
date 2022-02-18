c75b14542a6c31545c3cdf5189a2315cfabf54b2
Taisei Toya <tytaisei0102@gmail.com>
2022-02-18 22:58:22 +0100

    ex02

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,72 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array {
+ public:
+  Array();
+  Array(unsigned int len);
+  Array(Array const& src);
+  ~Array();
+
+  T& operator[](unsigned int n);
+  Array& operator=(Array const& rhs);
+
+  unsigned int size() const;
+
+ private:
+  T* value_;
+  unsigned int size_;
+};
+
+template <typename T>
+Array<T>::Array() {
+  value_ = new T[0]();
+  size_ = 0;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int len) {
+  value_ = new T[len]();
+  size_ = len;
+}
+
+template <typename T>
+Array<T>::Array(Array const& src) {
+  *this = src;
+}
+
+template <typename T>
+Array<T>::~Array() {
+  delete[] value_;
+}
+
+template <typename T>
+T& Array<T>::operator[](unsigned int n) {
+  if (n >= size_) {
+    throw std::exception();
+  }
+  return value_[n];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const {
+  return size_;
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(Array const& rhs) {
+  if (this != &rhs) {
+    delete[] this->value_;
+    this->value_ = new T[rhs.size_];
+    for (size_t i = 0; i < rhs.size_; i++) {
+      this->value_[i] = rhs.value_[i];
+    }
+    this->size_ = rhs.size_;
+  }
+  return *this;
+}
+
+#endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,50 @@
+#include "Array.hpp"
+
+int main(void) {
+  Array<int> int_array;
+
+  std::cout << "Size: " << int_array.size() << std::endl;
+
+  int_array = Array<int>(3);
+  std::cout << "Size: " << int_array.size() << std::endl;
+
+  for (size_t i = 0; i < int_array.size(); i++) {
+    std::cout << "int_array[" << i << "]: " << int_array[i] << std::endl;
+  }
+
+  int_array[0] = 1;
+  int_array[1] = 2;
+  int_array[2] = 3;
+
+  for (size_t i = 0; i < int_array.size(); i++) {
+    std::cout << "int_array[" << i << "]: " << int_array[i] << std::endl;
+  }
+
+  Array<int> int_array2;
+  int_array2 = int_array;
+
+  std::cout << "Size: " << int_array2.size() << std::endl;
+  for (size_t i = 0; i < int_array2.size(); i++) {
+    std::cout << "int_array2[" << i << "]: " << int_array2[i] << std::endl;
+  }
+
+  Array<int> int_array3 = Array<int>(int_array2);
+
+  int_array3[0] = 4;
+  int_array3[1] = 5;
+  int_array3[2] = 6;
+
+  std::cout << "Size: " << int_array3.size() << std::endl;
+  for (size_t i = 0; i < int_array3.size(); i++) {
+    std::cout << "int_array3[" << i << "]: " << int_array3[i] << std::endl;
+  }
+
+  std::cout << "Size: " << int_array2.size() << std::endl;
+  for (size_t i = 0; i < int_array2.size(); i++) {
+    std::cout << "int_array2[" << i << "]: " << int_array2[i] << std::endl;
+  }
+
+  std::cout << "-------------------" << std::endl;
+
+  return 0;
+}
