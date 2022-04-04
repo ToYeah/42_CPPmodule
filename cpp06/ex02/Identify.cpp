cf2c572af0199b3537595ff6cdac5084dc0666f8
Taisei Toya <tytaisei0102@gmail.com>
2022-04-04 13:07:22 +0200

    subjectに沿うよう修正

--- cpp06/ex02/Identify.cpp
+++ cpp06/ex02/Identify.cpp
@@ -27,13 +27,18 @@ void identify(Base* p) {
 };
 
 void identify(Base& p) {
-  Base a = p;
-
-  if (dynamic_cast<A*>(&p) != NULL) {
+  try {
+    A a = dynamic_cast<A&>(p);
     std::cout << "A" << std::endl;
-  } else if (dynamic_cast<B*>(&p) != NULL) {
-    std::cout << "B" << std::endl;
-  } else {
-    std::cout << "C" << std::endl;
+    return;
+  } catch (const std::exception& e) {
   }
+  try {
+    B b = dynamic_cast<B&>(p);
+    std::cout << "B" << std::endl;
+    return;
+
+  } catch (const std::exception& e) {
+  }
+  std::cout << "C" << std::endl;
 }
